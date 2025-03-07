ef6fce4ab25584b3483df6a2deb4d4c3e0f0cfe0
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-07 15:03:24 +0700

    refactor[tailwind.config.js]: Remove comment not used

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-=======
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +8,6 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
->>>>>>> e51a0f38321b240a305dfb034fbd14abdace5e47
   darkMode: "class",
   theme: {
     container: {
